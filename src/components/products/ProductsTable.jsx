8f31523004889596540a53a546e12195dde94075
My Name <hishamrehab309@gmail.com>
2024-09-29 04:06:39 +0300

    Products Table

--- src/components/products/ProductsTable.jsx
+++ src/components/products/ProductsTable.jsx
@@ -27,14 +27,6 @@ const ProductsTable = () => {
 
 
 
-
-
-
-
-
-
-
-
     return (
         <motion.div className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-5 border border-gray-700 mb-8"
             initial={{ opacity: 0, y: 20 }}
